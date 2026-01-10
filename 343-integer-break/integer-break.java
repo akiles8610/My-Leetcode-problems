c4b2b2d2f8ddd891416ddd5f2aea215025f56eec
AKILESWARAN S <146092578+akiles8610@users.noreply.github.com>
2026-01-10 21:59:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (40.21%) - LeetSync

---
+++ 343-integer-break/integer-break.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return  2;
+        int a=1;
+        while(n>4){
+            a=a*3;
+            n=n-3;
+        }
+        return a*n;
+        
+    }
+}
