854c36472a18c3a8e50e9830a72f3a1e721bdeae
AKILESWARAN S <146092578+akiles8610@users.noreply.github.com>
2026-01-23 12:36:03 +0530

    Time: 13 ms (25.06%) | Memory: 66.4 MB (5.39%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,31 +10,34 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-       int len=0;
-       ListNode temp=head;
-       while(temp!=null){
-        len++;
-        temp=temp.next;
-       }
-       int[] arr=new int[len];
-       temp=head;
-       int i=0;
-       while(temp!=null){
-        arr[i++]=temp.val;
-        temp=temp.next;
-       }
-       Arrays.sort(arr);
-       ListNode head1=null;
-       if(arr.length>0){
-         head1=new ListNode(arr[0]);
-        ListNode temp1=head1;
-        for(int j=1;j<arr.length;j++){
-            temp1.next=new ListNode(arr[j]);
-            temp1=temp1.next;
+        int len=0;
+        ListNode temp=head;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
         }
-       }
-       return head1;
+        int[] arr=new int[len];
+        temp=head;
+        int i=0;
+        while(temp!=null){
+            arr[i++]=temp.val;
+            temp=temp.next;
+        }
+        Arrays.sort(arr);
+        ListNode head2=null;
+        if(arr.length>0){
+        head2=new ListNode(arr[0]);
+        ListNode temp2=head2;
+        for(int j=1;j<arr.length;j++){
+            temp2.next=new ListNode(arr[j]);
+            temp2=temp2.next;
+            }
+        }
+        return head2;
 
+
+        
+      
 
 
 
